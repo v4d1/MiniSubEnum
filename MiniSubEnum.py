1cd35a60a63e9c7002e3f952740f8501e4aa0c71
v4d1 <alvaro.vadillo.av@gmail.com>
2022-02-11 11:31:14 +0000

    more compressed

--- MiniSubEnum.py
+++ MiniSubEnum.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env python
-#Usage python3 MiniSubEnum.py domain wordlist.txt --> example: python3 MiniSubEnum_threads.py facebook.com subdomains-5000.txt
 import socket, sys
-wordlist = open(sys.argv[2], 'r')
-entries = wordlist.readlines()
-for entry in entries:
+for line in open(sys.argv[2], 'r').readlines():
 	try:
-		socket.gethostbyname(entry.strip() +"."+ sys.argv[1])
-		print("Subdomain found: " + entry.strip() +"."+ sys.argv[1])
+		print(line.strip() +"." + sys.argv[1] + " at " + socket.gethostbyname(line.strip() +"." + sys.argv[1]))
 	except: pass
--- MiniSubEnum_threads.py
+++ MiniSubEnum_threads.py
@@ -1,15 +1,10 @@
-#!/usr/bin/env python
-#Usage python3 MiniSubEnum_threads.py domain wordlist.txt num_threads --> example: python3 MiniSubEnum_threads.py facebook.com subdomains-5000.txt 20
 import socket, sys, numpy as np
 from concurrent.futures import ThreadPoolExecutor
 def checkDomain(subdomains):
 	for subdomain in subdomains:
 		try:
-			socket.gethostbyname(subdomain.strip() +"."+ sys.argv[1])
-			print("Subdomain found: " + subdomain.strip() +"."+sys.argv[1])
+			print("Subdomain found: " + subdomain.strip() +"."+sys.argv[1] + " at " + socket.gethostbyname(subdomain.strip() +"."+ sys.argv[1]))
 		except: pass
 if __name__ == "__main__":
-	wordlist = open(sys.argv[2], 'r')
-	splited_list = np.array_split(wordlist.readlines(), int(sys.argv[3]))
-	executor = ThreadPoolExecutor(max_workers=int(sys.argv[3]))
-	[executor.submit(checkDomain, splited_list[i]) for i in range(int(sys.argv[3]))]
+	splited_list = np.array_split(open(sys.argv[2], 'r').readlines(), int(sys.argv[3]))
+	[ThreadPoolExecutor(max_workers=int(sys.argv[3])).submit(checkDomain, splited_list[i]) for i in range(int(sys.argv[3]))]
