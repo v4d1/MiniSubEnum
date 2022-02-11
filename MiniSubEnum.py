434df526802ea33f6a8bede7f6b8cf76538e0034
Vadi <alvaro.vadillo.av@gmail.com>
2022-02-11 11:51:20 +0000

    Update MiniSubEnum.py

--- MiniSubEnum.py
+++ MiniSubEnum.py
@@ -1,5 +1,5 @@
 import socket, sys
 for line in open(sys.argv[2], 'r').readlines():
 	try:
-		print(line.strip() +"." + sys.argv[1] + " at " + socket.gethostbyname(line.strip() +"." + sys.argv[1]))
+		print("Subdomain found: " + line.strip() +"." + sys.argv[1] + " at " + socket.gethostbyname(line.strip() +"." + sys.argv[1]))
 	except: pass
